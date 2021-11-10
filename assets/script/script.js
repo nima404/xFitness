8a0f566848b35d6dd28d442fff56bcdf589132de
nima tajik <nima.error404@gmail.com>
2021-11-10 13:46:33 -0800

    edt

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,16 @@
-
-
 function timNameClick() {
     document.getElementById("shHidTim").style.display = "block";
     document.getElementById("shHid").style.display = "none";
+    document.getElementById("learnDis").style.display = "none";
 }
 function homeClick() {
     document.getElementById("shHidTim").style.display = "none";
+    document.getElementById("learnDis").style.display = "none";
     document.getElementById("shHid").style.display = "block";
 }
+
+let LearnClick = document.querySelector("#videoClick").addEventListener("click" , function(){
+    document.getElementById("shHidTim").style.display = "none";
+    document.getElementById("learnDis").style.display = "block";
+    document.getElementById("shHid").style.display = "none";
+})
