f360824ac1057e6a2131a08618e85e24e376d3ac
nima tajik <nima.error404@gmail.com>
2021-11-10 03:48:32 -0800

    edt

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,3 +5,9 @@ let timClick = document.querySelector("#timNameClick").addEventListener("click" 
     document.querySelector(".shHid").classList.replace("shHid" , "showHiddenClick");
     document.querySelector(".shHidTim").classList.replace( "shHidTim" ,"shHidTimdis");
     })
+    
+
+    let HomeClick = document.querySelector("#homeClick").addEventListener("click" , function(){
+        document.querySelector(".shHid").classList.replace("showHiddenClick" , "disShow");
+        document.querySelector(".shHidTim").classList.replace( "shHidTimdis" ,"disShow");
+        })
