089bc698012f198cc7d16be4c6d8aba0e03d294a
nima tajik <nima.error404@gmail.com>
2021-11-15 01:46:17 -0800

    edt

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,3 +13,55 @@ let LearnClick = document.querySelector("#videoClick").addEventListener("click" 
     document.getElementById("learnDis").style.display = "block";
     document.getElementById("shHid").style.display = "none";
 })
+
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['javad', 'bsshahs', 'lkadnf'], 'text', ['tomato', 'orange', 'lightblue']);
+
+function consoleText(words, id, colors) {
+    if (colors === undefined) colors = ['#fff'];
+    var visible = true;
+    var con = document.getElementById('console');
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById(id)
+    target.setAttribute('style', 'color:' + colors[0])
+    window.setInterval(function () {
+
+        if (letterCount === 0 && waiting === false) {
+            waiting = true;
+            target.innerHTML = words[0].substring(0, letterCount)
+            window.setTimeout(function () {
+                var usedColor = colors.shift();
+                colors.push(usedColor);
+                var usedWord = words.shift();
+                words.push(usedWord);
+                x = 1;
+                target.setAttribute('style', 'color:' + colors[0])
+                letterCount += x;
+                waiting = false;
+            }, 1000)
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+            waiting = true;
+            window.setTimeout(function () {
+                x = -1;
+                letterCount += x;
+                waiting = false;
+            }, 1000)
+        } else if (waiting === false) {
+            target.innerHTML = words[0].substring(0, letterCount)
+            letterCount += x;
+        }
+    }, 120)
+    window.setInterval(function () {
+        if (visible === true) {
+            con.className = 'console-underscore hidden'
+            visible = false;
+
+        } else {
+            con.className = 'console-underscore'
+
+            visible = true;
+        }
+    }, 400)
+}
